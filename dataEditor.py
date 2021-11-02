98423bc074a9a1f5399c360adb4479ad013fd782
Thandi M <49248449+RealStr1ke@users.noreply.github.com>
2021-11-02 21:16:07 +0000

    Updated data and data retrieval script

--- dataEditor.py
+++ dataEditor.py
@@ -1,9 +1,19 @@
 import os
 import csv
 
+import urllib.request
+
+
 dataCDC = './src/cases.csv'
 stateInitials = ["al","ak","az","ar","ca","co","ct","de","dc","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
 def getData(folder, states):
+  if os.path.exists(dataCDC):
+    os.remove(dataCDC)
+  print('Retrieving CDC data...')
+  url = 'https://data.cdc.gov/api/views/9mfq-cb36/rows.csv?accessType=DOWNLOAD'
+  urllib.request.urlretrieve(url, './src/cases.csv')
+  print('Retrieved CDC data.')
+
   for state in states:
     file = state +"_cases.csv"
     filePath = os.path.join("/" + folder, file)
@@ -22,5 +32,5 @@ def getData(folder, states):
         reader = csv.reader(cases)
         for row in reader:
           if row[1].lower() == state:
-            writer.writerow({'date': row[0], 'new_cases': row[2]})
+            writer.writerow({'date': row[0], 'new_cases': row[5]})
 getData("data", stateInitials )
