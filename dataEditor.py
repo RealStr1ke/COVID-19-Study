39c7b609f68b1a9225ba11997e41558cbed936a6
Thandi M <TMenelas@outlook.com>
2021-11-03 15:51:41 -0400

    Manually sorted CDC data by date, fixed script and files

--- dataEditor.py
+++ dataEditor.py
@@ -7,19 +7,20 @@ import pandas as pd
 dataCDC = './assets/cases.csv'
 stateInitials = ["al","ak","az","ar","ca","co","ct","de","dc","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
 def getData(folder, states):
-  if os.path.exists(dataCDC):
-    os.remove(dataCDC)
-  print('Retrieving CDC data...')
-  url = 'https://data.cdc.gov/api/views/9mfq-cb36/rows.csv?accessType=DOWNLOAD'
-  wget.download(url, './src/cases.csv')
-  print('Retrieved CDC data.')
+  # if os.path.exists(dataCDC):
+  #   os.remove(dataCDC)
+  # print('Retrieving CDC data...')
+  # url = 'https://data.cdc.gov/api/views/9mfq-cb36/rows.csv?accessType=DOWNLOAD'
+  # wget.download(url, './src/cases.csv')
+  # print('Retrieved CDC data.')
 
-  csvCDC = csv.reader(dataCDC)
-  for row in csvCDC:
-    row[0] = (str(row[0])).replace("/", "-")
-  cdcData = pd.read_csv("./assets/cases.csv")
-  cdcData['submission_date'] = pd.to_datetime(cdcData.submission_date, infer_datetime_format = True)
-  cdcData.sort_values(by = 'submission_date', ascending = True, inplace = True)
+  # csvCDC = csv.reader(dataCDC)
+  # for row in csvCDC:
+  #   row[0] = (str(row[0])).replace("/", "-")
+  # cdcData = pd.read_csv("./assets/cases.csv")
+  # cdcData['submission_date'] = pd.to_datetime(cdcData.submission_date, infer_datetime_format = True)
+  # cdcData.sort_values(by = 'submission_date', ascending = True, inplace = True)
+  
   for state in states:
     file = state +"_cases.csv"
     filePath = os.path.join("/" + folder, file)
