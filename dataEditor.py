388f0508f7e019887b8a14ac660757ce1679a745
Thandi M <TMenelas@outlook.com>
2021-11-04 16:44:10 -0400

    Updating Python script in preparation to switch to JHU data

--- dataEditor.py
+++ dataEditor.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 dataCDC = './assets/cases.csv'
 stateInitials = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK", "OR", "PA","PR","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
-stateInitialsLowerCase = ["al","ak","az","ar","ca","co","ct","de","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
+stateInitialsL = ["al","ak","az","ar","ca","co","ct","de","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
 stateNames=["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 def getData(folder, names, initials):
   # if os.path.exists(dataCDC):
@@ -23,6 +23,8 @@ def getData(folder, names, initials):
   # cdcData['submission_date'] = pd.to_datetime(cdcData.submission_date, infer_datetime_format = True)
   # cdcData.sort_values(by = 'submission_date', ascending = True, inplace = True)
   
+  # download-directory.github.io?url=https://github.com/{org/user}/{repo}/tree/{branch}/{path}
+
   iNum = 0
   sNum = 0
 
@@ -43,9 +45,9 @@ def getData(folder, names, initials):
       with open(dataCDC, mode='r') as cases:
         reader = csv.reader(cases)
         for row in reader:
-          if row[1].lower() == state:
+          if row[1].lower() == names:
             writer.writerow({'Date': row[0], 'New Cases': row[5]})
     iNum = iNum + 1;
     sNum = sNum + 1;
 
-getData("data", stateNames, stateInitialsLowerCase)
+getData("data", stateNames, stateInitialsL)
