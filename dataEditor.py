7f978c5d610d433bd9b5b25bd73471cb5baa5d7f
Thandi M <TMenelas@outlook.com>
2021-11-04 14:40:41 -0400

    Updated Python script

--- dataEditor.py
+++ dataEditor.py
@@ -5,8 +5,10 @@ import pandas as pd
 
 
 dataCDC = './assets/cases.csv'
-stateInitials = ["al","ak","az","ar","ca","co","ct","de","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
-def getData(folder, states):
+stateInitials = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK", "OR", "PA","PR","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+stateInitialsLowerCase = ["al","ak","az","ar","ca","co","ct","de","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok", "or", "pa","pr","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy"]
+stateNames=["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+def getData(folder, names, initials):
   # if os.path.exists(dataCDC):
   #   os.remove(dataCDC)
   # print('Retrieving CDC data...')
@@ -21,7 +23,10 @@ def getData(folder, states):
   # cdcData['submission_date'] = pd.to_datetime(cdcData.submission_date, infer_datetime_format = True)
   # cdcData.sort_values(by = 'submission_date', ascending = True, inplace = True)
   
-  for state in states:
+  iNum = 0
+  sNum = 0
+
+  for initial in initials:
     file = state +"_cases.csv"
     filePath = os.path.join("/" + folder, file)
     if os.path.exists(filePath):
@@ -31,7 +36,7 @@ def getData(folder, states):
     newFilePath = "./" + folder + "/" + file
     os.rename("./" + file, newFilePath)
     with open(newFilePath, mode='w') as data:
-      headers = ['date', 'New Cases']
+      headers = ['Date', 'New Cases']
       writer = csv.DictWriter(data, fieldnames=headers)
 
       writer.writeheader()
@@ -39,5 +44,8 @@ def getData(folder, states):
         reader = csv.reader(cases)
         for row in reader:
           if row[1].lower() == state:
-            writer.writerow({'date': row[0], 'New Cases': row[5]})
-getData("data", stateInitials )
+            writer.writerow({'Date': row[0], 'New Cases': row[5]})
+    iNum = iNum + 1;
+    sNum = sNum + 1;
+
+getData("data", stateNames, stateInitialsLowerCase)
